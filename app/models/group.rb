e754e01e1031a03fdd89c6f6ed15637f9baf4f51
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-02 14:43:39 +0100

    added has_many through validations to pin and group models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :requests
+  has_many :users, through: :requests
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ApplicationRecord
   belongs_to :user
   belongs_to :resource
+  has_many :users, through: :resources
 end
