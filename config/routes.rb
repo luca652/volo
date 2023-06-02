1ea0b0b662a0e216772e8f7740262d8844e92fe1
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-02 16:21:44 +0100

    added routes for groups, requests, bookings, chatrooms, messages, resources, pins done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :groups, only: [:index, :show] do
+    POST "groups/:id/request", to: "requests#create"
+    POST "groups/:id/events/:id/booking", to: "bookings#create"
+    resources :chatrooms, only: [:show] do
+      resources :messages, only: [:create]
+    end
+  end
+  resources :resources, only: [:show] do
+    resources :pins, only: [:create]
+  end
 end
