21f1c1eda2ebb593f4232750f37842384550ba90
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-03 12:55:05 +0100

    populated seed file with one instance per class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Event.destroy_all
+Group.destroy_all
+User.destroy_all
+Resource.destroy_all
+
+
+user1 = User.create!( first_name: "Sara", last_name: "Carambola",
+                     language: "Italian", number_of_children: 2, childrens_age: "primary",
+                     location: "Canada Water", email: "sara@sara.com", password: "123456")
+
+
+group1 = Group.create!(name: "Bambini di Peckham", location: "Peckham Rye", language: "Italian", user_id: user1.id)
+
+event1 = Event.create!(name: "Crofton Park Story-Time in Italiano", location: "Crofton Park Library",
+                       category: "Books", date: "", user_id: user1.id, group_id: group1.id)
+
+resource1 = Resource.create!(title: "FABA - Music and stories in italian", category: "Stories", comment: "Changed my life!",
+                             picture_url: "#", user_id: user1.id)
