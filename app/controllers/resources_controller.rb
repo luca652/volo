dacb192dfe761e4f1fbe6a733e64354e2b49c3d5
zak freifer <freifery@gmail.com>
2023-06-04 12:21:40 +0100

    added updates to the filter

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,8 +1,9 @@
 class ResourcesController < ApplicationController
   def index
     @resources = Resource.all
-    
+    @categories = Resource.distinct.pluck(:category)
   end
+
 
   def show
     @resource = Resource.find(params[:id])
