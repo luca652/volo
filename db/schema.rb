f5cebb60b80f0245d4510cd7dc58364dcacfb193
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-06 20:38:32 +0100

    modified deafult value of declined and accepted fields in requests table - set it to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_160135) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_193649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,8 +83,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_160135) do
   create_table "requests", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "group_id", null: false
-    t.boolean "accepted"
-    t.boolean "declined"
+    t.boolean "accepted", default: false
+    t.boolean "declined", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_requests_on_group_id"
