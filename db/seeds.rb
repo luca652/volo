d65c888e36788def86b125772ad79f88b70739ec
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-08 13:52:19 +0100

    added 'requests' to seed file so our main group is populated with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Pin.destroy_all
 Resource.destroy_all
 User.destroy_all
 
-
+# USERS
 user1 = User.create!(first_name: "Sara", last_name: "Carambola", language: "Italian", number_of_children: 2,
                      childrens_age: "primary", location: "27, Plover Way, Canada Water, SE16 7TS",
                      email: "sara@me.com", password: "123456")
@@ -27,8 +27,6 @@ user3 = User.create!(first_name: "Jess", last_name: "Ferretti", language: "Itali
 user4 = User.create!(first_name: "Mario", last_name: "Pastorelli", language: "Italian", number_of_children: 1,
                      childrens_age: "pre-school", location: "10-14, Mercy Terrace, London SE13 7UX",
                      email: "mario@me.com", password: "123456")
-
-
 
 # GROUPS
 group1 = Group.create!(name: "Bambini di Peckham", location: "Peckham Rye", latitude: 51.46051,
@@ -51,6 +49,11 @@ group9 = Group.create!(name: "Brockwell Park - Estate Ragazzi", location: "Herne
                        longitude: -0.1075706, language: "Italian", user_id: user3.id)
 group10 = Group.create!(name: "Telegraph Hill - Doposcuola", location: "New Cross Gate", latitude: 51.468959,
                         longitude: -0.044985, language: "Italian", user_id: user4.id)
+# REQUESTS
+request1 = Request.create!(user_id: user1.id, group_id: group1.id, accepted: true)
+request2 = Request.create!(user_id: user2.id, group_id: group1.id, accepted: true)
+request3 = Request.create!(user_id: user3.id, group_id: group1.id, accepted: true)
+request4 = Request.create!(user_id: user4.id, group_id: group1.id, accepted: true)
 
 # EVENTS
 event1 = Event.create!(name: "Story-Time in Italiano", location: "Crofton Park Library, 375 Brockley Rd, London SE4 2AG",
