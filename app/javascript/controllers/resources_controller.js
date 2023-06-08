fb232f8ab71ac11c94946f3a67d9c22b12c26a01
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-08 20:58:14 +0100

    fixed issues with resources page

--- app/javascript/controllers/resources_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  connect() {
-    this.element.textContent = "resources World!"
-    console.log("Resources is working")
-  }
-}
