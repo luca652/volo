8ee8673efa88d2b9e2a175d1f42718b2258dd071
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-08 13:43:04 +0100

    added more resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,20 +55,16 @@ group10 = Group.create!(name: "Telegraph Hill - Doposcuola", location: "New Cros
 # EVENTS
 event1 = Event.create!(name: "Story-Time in Italiano", location: "Crofton Park Library, 375 Brockley Rd, London SE4 2AG",
                        category: "Reading", date: "2023-7-2", user_id: user2.id, group_id: group1.id)
-
 event2 = Event.create!(name: "Filastrocche nel parco", location: "Canada Water Library, 21 Surrey Quays Rd, London SE16 7AR",
                        category: "Activity", date: "2023-7-9", user_id: user3.id, group_id: group1.id)
-
 event3 = Event.create!(name: "Puppet teather", location: "Crofton Park Library, 375 Brockley Rd, London SE4 2AG",
                        category: "Activity", date: "2023-7-12", user_id: user4.id, group_id: group1.id)
 event4 = Event.create!(name: "Time to sing!", location: "Crofton Park Library, 375 Brockley Rd, London SE4 2AG",
                        category: "Music", date: "", user_id: user4.id, group_id: group1.id)
 event5 = Event.create!(name: "Sunday Morning Playclub", location: "Crofton Park Library, 375 Brockley Rd, London SE4 2AG",
                        category: "", date: "", user_id: user3.id, group_id: group1.id)
-
 event6 = Event.create!(name: "Picnic nel parco", location: "Hilly Fields, 375 Brockley Rd, London SE4 2AG",
                        category: "Activity", date: "2023-8-12", user_id: user2.id, group_id: group1.id)
-
 event7 = Event.create!(name: "Festa di compleanno di Andrea", location: "Crofton Park Library, 375 Brockley Rd, London SE4 2AG",
                        category: "Activity", date: "2023-7-30", user_id: user4.id, group_id: group1.id)
 
@@ -76,11 +72,26 @@ event7 = Event.create!(name: "Festa di compleanno di Andrea", location: "Crofton
 resource1 = Resource.create!(title: "FABA - Music and stories in italian", category: "Stories",
                              comment: "Utilissimo! Each character sings songs or tells a story in Italian. My daughter loves it!",
                              picture_url: "faba.png", user_id: user2.id)
-resource2 = Resource.create!(title: "Passeggino definitivo", category: "#", comment: "If you're planning to fly to Italy quite a bit, get this. It folds really small and you can take it on the plane",
+resource2 = Resource.create!(title: "Passeggino definitivo", category: "#",
+                             comment: "If you're planning to fly to Italy quite a bit, get this. It folds really small
+                             and you can take it on the plane",
                              picture_url: "pram.png", user_id: user3.id)
-resource3 = Resource.create!(title: "Le avventure di Cipollino", category: "reading", comment: "Un classico. I loved it when I was a kid!",
+resource3 = Resource.create!(title: "Le avventure di Cipollino", category: "reading",
+                             comment: "Un classico. I loved it when I was a kid!",
                              picture_url: "book.png", user_id: user4.id)
-resource4 = Resource.create!(title: "Talia e la valigia della idee", category: "#", comment: "Weekly Italian playgroups in London. Divertentissimo! Ve lo consiglio!",
+resource4 = Resource.create!(title: "Talia e la valigia della idee", category: "#",
+                             comment: "Weekly Italian playgroups in London. Divertentissimo! Ve lo consiglio!",
                              picture_url: "talia.png", user_id: user2.id)
-resource5 = Resource.create!(title: "Mappa di Torino", category: "#", comment: "Ciao! I bought this map for my daughter when we went to Torino for the weekend. She loved it!",
+resource5 = Resource.create!(title: "Mappa di Torino", category: "#",
+                             comment: "Ciao! I bought this map for my daughter when we went to Torino for the weekend. She loved it!",
                              picture_url: "mappa.png", user_id: user3.id)
+resource6 = Resource.create!(title: "TOPOLINO", category: "reading",
+                             comment: "Ho regalato un abbonamento a mio figlio e gli piace un sacco.
+                             Utilissimo per imparare parole nuove.", picture_url: "topolino.png", user_id: user3.id)
+resource7 = Resource.create!(title: "Nascondini", category: "food",
+                             comment: "Biscotti ecceziunali. The CostCutter near Brockley station stocks them.",
+                             picture_url: "biscotti.png", user_id: user4.id)
+resource8 = Resource.create!(title: "Mamma Dough Honor Oak", category: "food",
+                             comment: "Went here for a birthday party recently. Big place, very welcoming for children.
+                             They do a bunny rabbit pizza with ears which kids love.",
+                             picture_url: "pizza.png", user_id: user4.id)
