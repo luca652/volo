fca61729c455486c196572ab27ea00b65c8aecae
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-09 10:34:06 +0100

    added picture_url field to groups and resources with links to pictures in assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,25 +35,25 @@ user5 = User.create!(first_name: "Laura", last_name: "Martin", language: "Englis
 
 # GROUPS
 group1 = Group.create!(name: "Bambini di Peckham", location: "95A Rye Ln, London SE15 4ST",
-                       language: "Italian", user_id: user1.id)
+                       picture_url: "/groups/peckham2.png", language: "Italian", user_id: user1.id)
 group2 = Group.create!(name: "Brockley Playclub in Italiano", location: "1 Coulgate St, London SE4 2RW",
-                       language: "Italian", user_id: user2.id)
+                       picture_url: "/groups/brockley.png",language: "Italian", user_id: user2.id)
 group3 = Group.create!(name: "Quelli di Hilly Fields", location: "317 Brockley Rd, London SE4 2QZ",
-                       language: "Italian", user_id: user3.id)
+                       picture_url: "/groups/brockley.png", language: "Italian", user_id: user3.id)
 group4 = Group.create!(name: "Horniman Museum - Gruppo italiano", location: "100 London Rd, London SE23 3PQ",
-                       language: "Italian", user_id: user4.id)
+                       picture_url: "/groups/walrus.png", language: "Italian", user_id: user4.id)
 group5 = Group.create!(name: "Biblioteca di Canada Water", location: "21 Surrey Quays Rd, London SE16 7AR",
-                       language: "Italian", user_id: user2.id)
+                       picture_url: "/groups/canada.png", language: "Italian", user_id: user2.id)
 group6 = Group.create!(name: "Sydenham - Mayow Park", location: "313-315 Kirkdale, London SE26 4QB",
-                       language: "Italian", user_id: user3.id)
+                       picture_url: "/groups/peckham.png", language: "Italian", user_id: user3.id)
 group7 = Group.create!(name: "I dinosauri di Crystal Palace", location: "95 Church Rd, London SE19 2TA",
-                       language: "Italian", user_id: user4.id)
+                       picture_url: "/groups/crystalpalace.png", language: "Italian", user_id: user4.id)
 group8 = Group.create!(name: "Dulwich Village - Mamme e bambini", location: "116a Lordship Ln, London SE22 8HD",
-                       language: "Italian", user_id: user2.id)
+                       picture_url: "/groups/dulwich.png", language: "Italian", user_id: user2.id)
 group9 = Group.create!(name: "Brockwell Park - Estate Ragazzi", location: "10 Half Moon Ln, London SE24 9HU",
-                       language: "Italian", user_id: user3.id)
+                       picture_url: "/groups/brockwell.png", language: "Italian", user_id: user3.id)
 group10 = Group.create!(name: "Telegraph Hill - Doposcuola", location: "The Hill Station, Kitto Rd, London SE14 5TW",
-                        language: "Italian", user_id: user4.id)
+                        picture_url: "/groups/telegraph.png", language: "Italian", user_id: user4.id)
 
 # REQUESTS
 request1 = Request.create!(user_id: user1.id, group_id: group1.id, accepted: true)
@@ -91,27 +91,27 @@ booking7 = Booking.create!(event_id: event7.id, user_id: user1.id)
 #  RESOURCES
 resource1 = Resource.create!(title: "FABA - Music and stories in italian", category: "Stories",
                              comment: "Utilissimo! Each character sings songs or tells a story in Italian. My daughter loves it!",
-                             picture_url: "faba.png", user_id: user2.id)
+                             picture_url: "/resources/faba.png", user_id: user2.id)
 resource2 = Resource.create!(title: "Passeggino definitivo", category: "#",
                              comment: "If you're planning to fly to Italy quite a bit, get this. It folds really small
                              and you can take it on the plane",
-                             picture_url: "pram.png", user_id: user3.id)
+                             picture_url: "/resources/pram.png", user_id: user3.id)
 resource3 = Resource.create!(title: "Le avventure di Cipollino", category: "reading",
                              comment: "Un classico. I loved it when I was a kid!",
-                             picture_url: "book.png", user_id: user4.id)
+                             picture_url: "/resources/book.png", user_id: user4.id)
 resource4 = Resource.create!(title: "Talia e la valigia della idee", category: "#",
                              comment: "Weekly Italian playgroups in London. Divertentissimo! Ve lo consiglio!",
-                             picture_url: "talia.png", user_id: user2.id)
+                             picture_url: "/resources/talia.png", user_id: user2.id)
 resource5 = Resource.create!(title: "Mappa di Torino", category: "#",
                              comment: "Ciao! I bought this map for my daughter when we went to Torino for the weekend. She loved it!",
-                             picture_url: "mappa.png", user_id: user3.id)
+                             picture_url: "/resources/mappa.png", user_id: user3.id)
 resource6 = Resource.create!(title: "TOPOLINO", category: "reading",
                              comment: "Ho regalato un abbonamento a mio figlio e gli piace un sacco.
-                             Utilissimo per imparare parole nuove.", picture_url: "topolino.png", user_id: user3.id)
+                             Utilissimo per imparare parole nuove.", picture_url: "/resources/topolino.png", user_id: user3.id)
 resource7 = Resource.create!(title: "Nascondini", category: "food",
                              comment: "Biscotti ecceziunali. The CostCutter near Brockley station stocks them.",
-                             picture_url: "biscotti.png", user_id: user4.id)
+                             picture_url: "/resources/biscotti.png", user_id: user4.id)
 resource8 = Resource.create!(title: "Mamma Dough Honor Oak", category: "food",
                              comment: "Went here for a birthday party recently. Big place, very welcoming for children.
                              They do a bunny rabbit pizza with ears which kids love.",
-                             picture_url: "pizza.png", user_id: user4.id)
+                             picture_url: "/resources/pizza.png", user_id: user4.id)
