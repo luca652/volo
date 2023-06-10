ee73a7eb2a431f5873c4e24576d6cea488996a71
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-10 12:31:38 +0100

    fixed issue with chatroom controller - now a group has one chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomsController < ApplicationController
   def show
-    @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-    @group = Group.find(params[:group_id])
+    @group = Group.find(params[:id])
+    @chatroom = @group.chatroom
   end
 end
