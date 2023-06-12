ab42b0a03cd98e4cb14cc788291edeac1c0b494b
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-12 14:06:59 +0100

    added profile column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_131334) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_130548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_131334) do
     t.string "location"
     t.float "latitude"
     t.float "longitude"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
