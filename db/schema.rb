89440922b121359201270963c2efcdd3ba0eae6c
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-17 14:54:05 +0100

    added string column to stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_111245) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_134802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,6 +123,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_111245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
