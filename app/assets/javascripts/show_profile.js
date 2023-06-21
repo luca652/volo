9f9621e962c6cc055e2db53ab32d3564f1cd5eb4
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-21 11:21:24 +0100

    fixed issues with javascript for tabs

--- app/assets/javascripts/show_profile.js
+++ app/assets/javascripts/show_profile.js
@@ -2,13 +2,11 @@
 function openTab(event, tabName) {
   var i, tabcontent, tablinks;
 
-  // Hide all tab content
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the "active" class from all tab buttons
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -19,5 +17,21 @@ function openTab(event, tabName) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("defaultOpen").click();
+  document.getElementById("tablink-groups").click();
+});
+
+
+const groupsTab = document.getElementById("tablink-groups");
+groupsTab.addEventListener("click", (event) => {
+  openTab(event, "Groups");
+});
+
+const eventsTab = document.getElementById("tablink-events");
+eventsTab.addEventListener("click", (event) => {
+  openTab(event, "Events");
+});
+
+const pinsTab = document.getElementById("tablink-pins");
+pinsTab.addEventListener("click", (event) => {
+  openTab(event, "Pins");
 });
