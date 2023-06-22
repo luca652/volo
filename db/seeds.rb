55fe209b0fb49f99cfc1379bae5b77acd7f44c89
luca gastaldi <lucagastaldi82@gmail.com>
2023-06-22 16:53:43 +0100

    Made length of comments similar to have pins dislpay well

--- db/seeds.rb
+++ db/seeds.rb
@@ -139,20 +139,20 @@ resource2 = Resource.create!(title: "Passeggino definitivo", category: "General"
                              and you can take it on the plane",
                              picture_url: "resources/pram.png", user_id: user3.id)
 resource3 = Resource.create!(title: "Favole al telefono", category: "Stories",
-                             comment: "Un classico. I loved it when I was a kid!",
-                             picture_url: "resources/rodari.png", user_id: user4.id)
+                             comment: "Un classico. L'ho letto quando andavo alle elementari! Ho cominciato a leggerlo
+                             a mia figlia e le piace molto!", picture_url: "resources/rodari.png", user_id: user4.id)
 resource4 = Resource.create!(title: "Talia e la valigia della idee", category: "#",
-                             comment: "Weekly Italian playgroup_images in London. Divertentissimo! Ve lo consiglio!",
-                             picture_url: "resources/talia.png", user_id: user2.id)
+                             comment: "Weekly Italian playgroup in London. I bambini lo adorano e ci vogliono andare
+                             tutte le settimane. Ve lo consiglio!", picture_url: "resources/talia.png", user_id: user2.id)
 resource5 = Resource.create!(title: "Il libro degli errori", category: "Stories",
-                             comment: "Ciao! I bought this map for my daughter when we went to Torino for the weekend. She loved it!",
-                             picture_url: "resources/rodari2.png", user_id: user3.id)
+                             comment: "Ciao! Ho letto tutto Gianni Rodari quando ero piccola e adesso li sto
+                             comprando anche ai bambini. Poi sono piemontese...", picture_url: "resources/rodari2.png", user_id: user3.id)
 resource6 = Resource.create!(title: "Topolino", category: "Stories",
                              comment: "Ho regalato un abbonamento a mio figlio e gli piace un sacco.
                              Utilissimo per imparare parole nuove.", picture_url: "resources/topolino.png", user_id: user3.id)
 resource7 = Resource.create!(title: "Indagatore dell'incubo", category: "Stories",
-                             comment: "Non per bambini ma per ragazzi/ragazze. Io ce li avevo tutti!",
-                             picture_url: "resources/dylandog.png", user_id: user4.id)
+                             comment: "Non per bambini ma per ragazzi/ragazze. Io ce li avevo tutti! È ambientato a Londra,
+                             cosa che ai miei figli piace molto!", picture_url: "resources/dylandog.png", user_id: user4.id)
 resource8 = Resource.create!(title: "Mamma Dough Honor Oak", category: "Food",
                              comment: "Went here for a birthday party recently. Big place, very welcoming for children.
                              They do a bunny rabbit pizza with ears.",
