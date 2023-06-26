492be9e0f6a958d832477d352180da14a73da41f
Vittoria Sajeva vittoria.sajeva@gmail.com <vittoria.sajeva@gmail.com>
2023-06-26 16:54:21 +0100

    repositioning of the search and filter bars

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -37,6 +37,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 18, maxZoom: 100, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 100, duration: 0 })
   }
 }
