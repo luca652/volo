e151291f52fb54264546b6b8c91b6e0a9c1ceed1
Vittoria Sajeva vittoria.sajeva@gmail.com <vittoria.sajeva@gmail.com>
2023-06-29 20:01:45 +0100

    changes

--- db/migrate/20230627180959_remove_goal_from_prompts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveGoalFromPrompts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :prompts, :goal
-  end
-end
--- db/migrate/20230627181147_add_language_to_prompts.rb
+++
@@ -1,5 +0,0 @@
-class AddLanguageToPrompts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :prompts, :language, :string
-  end
-end
