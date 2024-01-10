02d75af54b6211c7755a0737dc712cf59d7db205
luca gastaldi <lucagastaldi82@gmail.com>
2024-01-10 16:53:24 +0000

    added create method to resources controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,15 +4,27 @@ class ResourcesController < ApplicationController
     @categories = Resource.distinct.pluck(:category)
   end
 
-
   def show
     @resource = Resource.find(params[:id])
   end
 
-  def pin
-    @resource = Resource.includes(:pins).find(params[:id])
-    
-    current_user.pins.create(resource: @resource)
-    # ...
+  def new
+    @resource = Resource.new
+  end
+
+  def create
+    @resource = Resource.new(resource_params)
+
+    if @resource.save
+      redirect_to resources_path, notice: "Resource was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:title, :category, :comment, :picture_url, :user_id)
   end
 end
