bf3ca97b2ad5f98c17f8248152a1c85e6bee93d1
luca gastaldi <lucagastaldi82@gmail.com>
2024-01-11 10:14:23 +0000

    added title validation to resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,6 +2,8 @@ class Resource < ApplicationRecord
   has_many :pins, dependent: :destroy
   has_many :users, through: :pins
 
+  validates :title, presence: true
+
   def pin_count
     pins.count
   end
