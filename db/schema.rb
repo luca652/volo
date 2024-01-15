c065e68d479a9021c106413988b0b9ee8bb5e58e
luca gastaldi <lucagastaldi82@gmail.com>
2024-01-15 15:37:03 +0000

    removed pin_count column from resources table - the way we count pins is by counting the instances of Pin associated with the resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_200445) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_15_153410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,7 +114,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_200445) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "pin_count", default: 0
     t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
