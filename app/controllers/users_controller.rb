4e658425a73ed7ce801f6bb09f68ab7ef3bb9701
luca gastaldi <lucagastaldi82@gmail.com>
2024-01-31 10:25:51 +0000

    tidied up users controller using eager loading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,13 @@ class UsersController < ApplicationController
   def show_profile
 
     @user = current_user
-
     # GROUPS
     # groups where user is an admin
     @groups_admin = Group.where(user_id: @user.id)
-
     # groups where user is a member
-    @requests = Request.where(user_id: @user.id)
-    @groups_member = []
-    @requests.each do |request|
-      if request.accepted == true
-        @groups_member.push(request.group)
-      end
-    end
-
-    # user's groups
+    @accepted_requests = Request.where(user_id: @user.id, accepted: true)
+    @groups_member = Group.joins(:requests).where(requests: { id: @accepted_requests.pluck(:id) })
+    # all user's groups
     @groups = @groups_admin + @groups_member
 
     # EVENTS
