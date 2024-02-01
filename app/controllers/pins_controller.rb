64639da410f9e310803fe5b098249e7d5fd125d6
luca gastaldi <lucagastaldi82@gmail.com>
2024-02-01 11:50:27 +0000

    changed iteration to db query with join to retrieve @resources in users controller show_profile acttion

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,32 +4,11 @@ class PinsController < ApplicationController
     @pins = current_user.pins.includes(:resource)
   end
 
-  # def create
-  #   @resource = Resource.find(params[:resource_id])
-
-  #   if current_user.has_pinned?(@resource)
-  #     render json: { error: 'Resource already pinned' }, status: :unprocessable_entity
-  #   else
-  #     pin = current_user.pins.build(resource: @resource)
-
-  #     if pin.save
-  #       render json: { resource_id: @resource.id, pin_count: @resource.pin_count }, status: :created
-  #     else
-  #       render json: { error: pin.errors.full_messages }, status: :unprocessable_entity
-  #     end
-  #   end
-  # end
-
-
-
-
-
   def destroy
     @pin = current_user.pins.find(params[:id])
     @pin.destroy
     redirect_to pins_path, notice: 'Pin removed successfully!'
   end
-
 
   def pin_unpin
     @resource = Resource.find(params[:resource_id])
