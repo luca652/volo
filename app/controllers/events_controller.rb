4a459f260ddefd576e9ef196fbff6401e6b840fd
luca gastaldi <lucagastaldi82@gmail.com>
2024-02-02 13:04:59 +0000

    user can now add an event on the group show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,17 +4,21 @@ class EventsController < ApplicationController
   end
 
   def new
-    @group = Group.find(params[group_id:])
+    @group = Group.find(params[:group_id])
     @event = Event.new
-    @group = @event.group
-  end
-
-  def new_group_event
-    @event = Event.new
-    @groups = current_user.groups
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @event = Event.new(event_params)
+    @event.group = @group
+
+    if @event.save
+      @booking = Booking.create(event_id: @event.id, user_id: current_user.id)
+      redirect_to group_path(@group), notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -24,5 +28,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :category, :description, :date, :group_id, :user_id)
   end
 end
