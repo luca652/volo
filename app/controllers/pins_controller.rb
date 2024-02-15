2d8f9aa91ded86ab355f5cf4a246783da5f23d08
luca gastaldi <lucagastaldi82@gmail.com>
2024-02-15 13:26:34 +0000

    fixed issue with redirect when unpinning from user/show_profile

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -16,11 +16,11 @@ class PinsController < ApplicationController
     if current_user.has_pinned?(@resource)
       @pin = current_user.pins.find_by(resource: @resource)
       @pin.destroy
-      redirect_to resources_path
+      redirect_back fallback_location: resources_path
     else
       @pin = Pin.new(user: current_user, resource: @resource)
       if @pin.save
-        redirect_to resources_path
+        redirect_back fallback_location: resources_path
       else
         render json: { error: pin.errors.full_messages }, status: :unprocessable_entity
       end
