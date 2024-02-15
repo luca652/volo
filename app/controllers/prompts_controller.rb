d8f710555b25dadc4cae3ed49f1d21b5d651a7c4
luca gastaldi <lucagastaldi82@gmail.com>
2024-02-15 10:31:04 +0000

    removed instantiation of open ai client from generate_ai_story method in prompts controller

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,7 +1,6 @@
 class PromptsController < ApplicationController
 
   def create
-    raise
     @prompt = Prompt.new(prompt_params)
     @user = @prompt.user
 
@@ -20,11 +19,10 @@ class PromptsController < ApplicationController
   end
 
   def generate_ai_story(prompt)
-    @openai_client = OpenAI::Client.new
     request = "Tell me a story of maximum 250 words in #{prompt.language}. The protagonist is #{prompt.protagonist},
     and the story is set in #{prompt.setting}. His mortal enemy is #{prompt.enemy}, and his/her favorite food is #{prompt.food}.
     Start with a title, something like 'Arthur and the dark forest'. Put a # at the end of the title."
-    response = @openai_client.chat(parameters: { model: "gpt-3.5-turbo", messages: [{ role: "user", content: request }],
+    response = $open_ai_client.chat(parameters: { model: "gpt-3.5-turbo", messages: [{ role: "user", content: request }],
                                                   temperature: 0.7 })
     generated_story = response.dig("choices", 0, "message", "content")
 
